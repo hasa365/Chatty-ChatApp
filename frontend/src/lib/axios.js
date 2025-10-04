051ddfaa74aafc3c8dbcef873d84aa7521be15bb
Hasa365 <hasanthi916@gmail.com>
2025-10-05 00:17:22 +0530

    remove /api from frontend urls

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" 
-    ? `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api` 
-    : `${import.meta.env.VITE_API_URL}/api`,
+    ? `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}` 
+    : `${import.meta.env.VITE_API_URL}`,
     withCredentials: true,
 });
 
